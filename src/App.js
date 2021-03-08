bf8b4888397ee1d1472d72cdf44d08cd53a67781
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-08 17:22:15 -0500

    add header tags to App.js, add header styling to App.css

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <nav>
-         <h2>Shop</h2>
-         <ul>
-           <li><a href="#">Home</a></li>
-           <li><a href="#">About</a></li>
-           <li><a href="#">Cart </a></li>
-         </ul>
-       </nav>
-
+      <header>
+        <nav>
+          <h2>Shop</h2>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Cart </a></li>
+          </ul>
+        </nav>
+       </header>
     </div>
   );
 }
