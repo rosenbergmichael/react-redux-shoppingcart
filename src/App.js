95e48a2803dedee9da88559aef579da641661b93
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-12 15:36:20 -0500

    move code for homepage from App.js to Home.js, in App.js import Home component and render

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <div className="overlay"></div>
-        <nav>
-          <h2>Shop</h2>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li className="cart"><a href="#">
-            <ion-icon name="basket"></ion-icon>Cart<span> (0)</span>
-              </a></li>
-          </ul>
-        </nav>
-       </header>
+      <Home />
     </div>
   );
 }
