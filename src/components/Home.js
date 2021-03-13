2443639c429f8c6cbb913f1a434513475b731fa5
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-13 17:24:14 -0500

    in Home.js add four div's for products, with className image, h3 for title, h3 for price and link for add to cart

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,30 @@
  const Home = () => {
   return(
     <div className="container">
-      
+      <div className="image">
+        <img src="" alt="" />
+        <h3>Tshirt</h3>
+        <h3>$20.00 </h3>
+        <a href="#">Add to Cart</a>
+      </div>
+      <div className="image">
+        <img src="" alt="" />
+        <h3>Tshirt</h3>
+        <h3>$20.00 </h3>
+        <a href="#">Add to Cart</a>
+      </div>
+      <div className="image">
+        <img src="" alt="" />
+        <h3>Tshirt</h3>
+        <h3>$20.00 </h3>
+        <a href="#">Add to Cart</a>
+      </div>
+      <div className="image">
+        <img src="" alt="" />
+        <h3>Tshirt</h3>
+        <h3>$20.00 </h3>
+        <a href="#">Add to Cart</a>
+      </div>
     </div>
   );
 };
