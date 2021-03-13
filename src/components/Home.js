170c7ab671685545dfa644e1c22ca0ba764e9d19
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-13 17:40:10 -0500

    in Home.js import jpeg files, add them to img src tags with description for alt tags

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,32 @@
  import React from 'react'; 
+ import giantsHat from '../images/giantshat.jpeg'
+ import giantsShorts from '../images/giantsshorts.jpeg'
+ import metsJersey from '../images/metsjersey.jpeg'
+ import bullsHat from '../images/bullshat.jpeg'
  
  const Home = () => {
   return(
     <div className="container">
       <div className="image">
-        <img src="" alt="" />
+        <img src={giantsHat} alt="NY Giants Hat" />
         <h3>Tshirt</h3>
         <h3>$20.00 </h3>
         <a href="#">Add to Cart</a>
       </div>
       <div className="image">
-        <img src="" alt="" />
+        <img src={giantsShorts} alt="NY Giants Shorts" /> 
         <h3>Tshirt</h3>
         <h3>$20.00 </h3>
         <a href="#">Add to Cart</a>
       </div>
       <div className="image">
-        <img src="" alt="" />
+        <img src={metsJersey} alt="NY Mets Jersey" />
         <h3>Tshirt</h3>
         <h3>$20.00 </h3>
         <a href="#">Add to Cart</a>
       </div>
       <div className="image">
-        <img src="" alt="" />
+        <img src={bullsHat} alt="Chicago Bulls Hat" />
         <h3>Tshirt</h3>
         <h3>$20.00 </h3>
         <a href="#">Add to Cart</a>
