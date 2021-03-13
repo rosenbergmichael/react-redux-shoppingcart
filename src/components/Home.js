cbeb2c34a0652cd8ec01bc8a23abaf283383f828
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-13 18:23:28 -0500

    in Home.js add cart number to a tag classNames, in App.css add styling for addToCart and image hover

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,25 +11,25 @@
         <img src={giantsHat} alt="NY Giants Hat" />
         <h3>NY Giants Hat</h3>
         <h3>$15.00 </h3>
-        <a className="addToCart" href="#">Add to Cart</a>
+        <a className="addToCart cart1" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={giantsShorts} alt="NY Giants Shorts" /> 
         <h3>NY Giants Shorts</h3>
         <h3>$40.00 </h3>
-        <a className="addToCart" href="#">Add to Cart</a>
+        <a className="addToCart cart2" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={metsJersey} alt="NY Mets Jersey" />
         <h3>NY Mets Jersey</h3>
         <h3>$70.00 </h3>
-        <a className="addToCart" href="#">Add to Cart</a>
+        <a className="addToCart cart3" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={bullsHat} alt="Chicago Bulls Hat" />
         <h3>Chicago Bulls Hat</h3>
         <h3>$35.00 </h3>
-        <a className="addToCart" href="#">Add to Cart</a>
+        <a className="addToCart cart4" href="#">Add to Cart</a>
       </div>
     </div>
   );
