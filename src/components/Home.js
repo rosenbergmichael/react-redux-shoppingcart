9169f670bff9b5a4e6a939f80adf4ef7b8acd1d8
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-14 17:10:28 -0400

    in Home.js delete useState, addToBasket function and onClicks in product a tags in order to set up this functionality with Redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,11 @@
  
  const Home = () => {
 
-  const [basketNumbers, setBasketNumbers] = useState(0);
+  // const [basketNumbers, setBasketNumbers] = useState(0);
 
-  const addToBasket = () => {
-    setBasketNumbers(basketNumbers + 1);
-  }
+  // const addToBasket = () => {
+  //   setBasketNumbers(basketNumbers + 1);
+  // }
 
 
   return(
@@ -20,27 +20,26 @@
         <img src={giantsHat} alt="NY Giants Hat" />
         <h3>NY Giants Hat</h3>
         <h3>$15.00 </h3>
-        <a onClick={addToBasket} className="addToCart cart1" href="#">Add to Cart</a>
+        <a className="addToCart cart1" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={giantsJacket} alt="NY Giants Shorts" /> 
         <h3>NY Giants Jacket</h3>
         <h3>$90.00 </h3>
-        <a onClick={addToBasket} className="addToCart cart2" href="#">Add to Cart</a>
+        <a className="addToCart cart2" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={metsJersey} alt="NY Mets Jersey" />
         <h3>NY Mets Jersey</h3>
         <h3>$70.00 </h3>
-        <a onClick={addToBasket} className="addToCart cart3" href="#">Add to Cart</a>
+        <a className="addToCart cart3" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={bullsHat} alt="Chicago Bulls Hat" />
         <h3>Chicago Bulls Hat</h3>
         <h3>$35.00 </h3>
-        <a onClick={addToBasket} className="addToCart cart4" href="#">Add to Cart</a>
+        <a className="addToCart cart4" href="#">Add to Cart</a>
       </div>
-      <h1>Current Number in Cart {basketNumbers}</h1>
     </div>
   );
 };
