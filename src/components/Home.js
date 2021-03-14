4ccaf83be2e90fbb91cfb0ff094c54e254dea2c1
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-14 17:02:13 -0400

    in Home.js create addToBasket function, add onClick with this function to each product a tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,10 @@
 
   const [basketNumbers, setBasketNumbers] = useState(0);
 
-  
+  const addToBasket = () => {
+    console.log("button clicked")
+  }
+
 
   return(
     <div className="container">
@@ -17,25 +20,25 @@
         <img src={giantsHat} alt="NY Giants Hat" />
         <h3>NY Giants Hat</h3>
         <h3>$15.00 </h3>
-        <a className="addToCart cart1" href="#">Add to Cart</a>
+        <a onClick={addToBasket} className="addToCart cart1" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={giantsJacket} alt="NY Giants Shorts" /> 
         <h3>NY Giants Jacket</h3>
         <h3>$90.00 </h3>
-        <a className="addToCart cart2" href="#">Add to Cart</a>
+        <a onClick={addToBasket} className="addToCart cart2" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={metsJersey} alt="NY Mets Jersey" />
         <h3>NY Mets Jersey</h3>
         <h3>$70.00 </h3>
-        <a className="addToCart cart3" href="#">Add to Cart</a>
+        <a onClick={addToBasket} className="addToCart cart3" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={bullsHat} alt="Chicago Bulls Hat" />
         <h3>Chicago Bulls Hat</h3>
         <h3>$35.00 </h3>
-        <a className="addToCart cart4" href="#">Add to Cart</a>
+        <a onClick={addToBasket} className="addToCart cart4" href="#">Add to Cart</a>
       </div>
     </div>
   );
