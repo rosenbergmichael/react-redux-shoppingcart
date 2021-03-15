2405adbfa14bb9fb87ee564e02bff9307317c37e
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-15 10:18:03 -0400

    in store.js create variable store, set to createStore with rootReducer, initialState and composeWithDevTools with applyMiddleware and middleware

--- src/store.js
+++ src/store.js
@@ -5,3 +5,9 @@ import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
+
+const store =  createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware));
+);
