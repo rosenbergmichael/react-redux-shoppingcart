c4cab6e8af5cbe7e46a51affc93a4095da698b0b
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-16 08:46:32 -0400

    in App.js import Provider from react-redux, wrap App component in Provider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import './App.css';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
