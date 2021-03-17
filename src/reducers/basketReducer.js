c458f83207f7454a55ac3dd5ee3f40a1246c6751
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-17 16:58:35 -0400

    in basketReducer.js create case for ADD_PRODUCT_BASKET, set return state equal to this.basketNumbers + 1

--- src/reducers/basketReducer.js
+++ src/reducers/basketReducer.js
@@ -1,9 +1,15 @@
+import { ADD_PRODUCT_BASKET } from "../actions/types";
+
 const initialState = {
   basketNumbers: 0
 }
 
 export default(state = initialState, action) => {
    switch(action.type) {
+     case ADD_PRODUCT_BASKET: 
+      return {
+        basketNumbers: this.basketNumbers + 1
+      }
       default: 
         return state; 
    }
