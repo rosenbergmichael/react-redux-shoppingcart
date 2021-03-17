1380e231ea4e8063a1ec472f62f6d0c8a7040657
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-17 16:49:36 -0400

    in basketReducer.js for state parameter set equal to initialState

--- src/reducers/basketReducer.js
+++ src/reducers/basketReducer.js
@@ -2,9 +2,9 @@ const initialState = {
   basketNumbers: 0
 }
 
-export default(state, action) => {
+export default(state = initialState, action) => {
    switch(action.type) {
       default: 
-        return state;
+        return state; 
    }
 }
