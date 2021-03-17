24e77f136f18feae095fa67fb4ddd5d1cd15619b
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-17 16:46:01 -0400

    in Home.js pass in props, for each a tag add onClick equal to props.addBasket

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,33 +7,33 @@ import { connect } from 'react-redux';
 import { addBasket } from '../actions/addAction';
 
  
- const Home = () => {
- 
+ const Home = (props) => {
+  console.log(props);
   return(
     <div className="container">
       <div className="image">
         <img src={giantsHat} alt="NY Giants Hat" />
         <h3>NY Giants Hat</h3>
         <h3>$15.00 </h3>
-        <a className="addToCart cart1" href="#">Add to Cart</a>
+        <a onClick={props.addBasket} className="addToCart cart1" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={giantsJacket} alt="NY Giants Shorts" /> 
         <h3>NY Giants Jacket</h3>
         <h3>$90.00 </h3>
-        <a className="addToCart cart2" href="#">Add to Cart</a>
+        <a onClick={props.addBasket} className="addToCart cart2" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={metsJersey} alt="NY Mets Jersey" />
         <h3>NY Mets Jersey</h3>
         <h3>$70.00 </h3>
-        <a className="addToCart cart3" href="#">Add to Cart</a>
+        <a onClick={props.addBasket} className="addToCart cart3" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={bullsHat} alt="Chicago Bulls Hat" />
         <h3>Chicago Bulls Hat</h3>
         <h3>$35.00 </h3>
-        <a className="addToCart cart4" href="#">Add to Cart</a>
+        <a onClick={props.addBasket}  className="addToCart cart4" href="#">Add to Cart</a>
       </div>
     </div>
   );
