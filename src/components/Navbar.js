1aaf3afa5e2178d221d4ead4750a9d721846e871
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-18 07:58:14 -0400

    in Navbar.js import Link from react-router-dom, replace anchor tag hrefs with appropriate Link to's for Navbar menu options

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useEffect } from 'react';
 import  { connect } from 'react-redux';
 import { getNumbers } from '../actions/getAction';
-
+import { Link } from 'react-router-dom';
 
 function Navbar(props ) {
 
@@ -15,11 +15,11 @@ function Navbar(props ) {
         <nav>
           <h2>Shop</h2>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li className="cart"><a href="#">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li className="cart"><Link to="/cart">
             <ion-icon name="basket"></ion-icon>Cart<span> ({props.basketProps.basketNumbers})</span>
-              </a></li>
+              </Link></li>
           </ul>
         </nav>
        </header>
