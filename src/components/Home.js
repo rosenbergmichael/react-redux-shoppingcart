c42c260bd42f70143d52b776ee1a5fddc6b0781f
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-22 10:09:53 -0400

    in Home.js for each product's onClick add arrow function to get product info for adding to basket

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,25 +15,25 @@ import { addBasket } from '../actions/addAction';
         <img src={giantsHat} alt="NY Giants Hat" />
         <h3>NY Giants Hat</h3>
         <h3>$15.00 </h3>
-        <a onClick={props.addBasket} className="addToCart cart1" href="#">Add to Cart</a>
+        <a onClick={() => props.addBasket('giantsHat')} className="addToCart cart1" href="#">Add to Cart</a>
       </div>
       <div className="image">
-        <img src={giantsJacket} alt="NY Giants Shorts" /> 
+        <img src={giantsJacket} alt="NY Giants Jacket" /> 
         <h3>NY Giants Jacket</h3>
         <h3>$90.00 </h3>
-        <a onClick={props.addBasket} className="addToCart cart2" href="#">Add to Cart</a>
+        <a onClick={() => props.addBasket('giantsJacket')} className="addToCart cart2" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={metsJersey} alt="NY Mets Jersey" />
         <h3>NY Mets Jersey</h3>
         <h3>$70.00 </h3>
-        <a onClick={props.addBasket} className="addToCart cart3" href="#">Add to Cart</a>
+        <a onClick={() => props.addBasket('metsJersey')} className="addToCart cart3" href="#">Add to Cart</a>
       </div>
       <div className="image">
         <img src={bullsHat} alt="Chicago Bulls Hat" />
         <h3>Chicago Bulls Hat</h3>
         <h3>$35.00 </h3>
-        <a onClick={props.addBasket}  className="addToCart cart4" href="#">Add to Cart</a>
+        <a onClick={() => props.addBasket('bullsHat')}  className="addToCart cart4" href="#">Add to Cart</a>
       </div>
     </div>
   );
