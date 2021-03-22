e7b38a06ac6f35a6379355bb389c069ca197a6a7
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-22 10:18:48 -0400

    in basketReducer.js add to initialState a cartCost of 0 and a products object containing key value pairs for the four products with their name, price, starting number of 0 and inCart set to false

--- src/reducers/basketReducer.js
+++ src/reducers/basketReducer.js
@@ -1,7 +1,34 @@
 import { ADD_PRODUCT_BASKET, GET_BASKET_NUMBERS } from "../actions/types";
 
 const initialState = {
-  basketNumbers: 0
+  basketNumbers: 0,
+  cartCost: 0,
+  products: {
+    giantsHat: {
+      name: 'NY Giants Hat',
+      price: 15.00,
+      numbers: 0,
+      inCart: false
+    },
+    giantsJacket: {
+      name: 'NY Giants Jacket',
+      price: 90.00,
+      numbers: 0,
+      inCart: false
+    },
+    metsJersey: {
+      name: 'NY Mets Jersey',
+      price: 70.00,
+      numbers: 0,
+      inCart: false
+    },
+    bullsHat: {
+      name: 'Chicago Bulls Hat',
+      price: 35.00,
+      numbers: 0,
+      inCart: false
+    },
+  }
 }
 
 export default(state = initialState, action) => {
