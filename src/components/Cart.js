0b41db9fe3a68767256d37c785b7d4e73fada8f1
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-22 11:17:10 -0400

    in Cart.js inside productsInCart.map build out return divs for product image, name, quantity, price and total, import image files

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,9 @@
 import React, {Fragment} from 'react';
 import { connect } from 'react-redux';
+import giantsHat from '../images/giantshat.jpeg';
+import giantsJacket from '../images/giantsjacket.jpg';
+import metsJersey from '../images/metsjersey.jpeg';
+import bullsHat from '../images/bullshat.jpeg';
 
 function Cart({basketProps}) {
   console.log(basketProps);
@@ -18,7 +22,16 @@ function Cart({basketProps}) {
   productsInCart = productsInCart.map( (product, index) => {
     return (
       <Fragment>
-        
+        <div className="product"><ion-icon name="close-circle"></ion-icon><img src={productImages[index]} />
+          <span className="sm-hide">{product.name}</span>
+        </div>
+        <div className="price sm-hide">${product.price}.00</div>
+        <div className="quantity">
+          <ion-icon className="decrease" name="arrow-back-circle-outline"></ion-icon>
+            <span>{product.numbers}</span>
+          <ion-icon className="increase" name="arrow-forward-circle-outline"></ion-icon>  
+        </div>
+        <div className="total">${product.numbers * product.price}.00</div>
       </Fragment>
     )
   });
