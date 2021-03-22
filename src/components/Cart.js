0a4bac48980db89fac061e2a7e8c551bceb7440f
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-22 11:01:32 -0400

    in Cart.js pass in basketProps to Cart function, set productsInCart variable to empty array, Object.keys.forEach to get product inCart status, and if statement to push product into productsInCart if inCart is true

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-function Cart() {
+function Cart({basketProps}) {
+  console.log(basketProps);
+
+  let productsInCart = [];
+
+  Object.keys(basketProps.products).forEach( function(item) {
+    console.log(item);
+    console.log(basketProps.products[item].inCart);
+    if(basketProps.products[item].inCart) {
+      productsInCart.push(basketProps.products[item])
+    }
+    console.log(productsInCart);
+  })
+
   return (
     <div>
       <h1>This is the Cart</h1>
@@ -10,7 +23,7 @@ function Cart() {
 }
 
 const mapStateToProps = state => ({
-  basketProps = state.basketState 
+  basketProps: state.basketState 
 });
 
 export default connect(mapStateToProps)(Cart);
