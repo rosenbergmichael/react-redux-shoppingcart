cc38e89354f791d74b19255092f51d386589b433
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-23 10:20:44 -0400

    in productQuantity.js create and export productQuantity function passing in action and name, with return statement passing in dispatch

--- src/actions/productQuantity.js
+++ src/actions/productQuantity.js
@@ -1,1 +1,7 @@
 import { INCREASE_QUANTITY, DECREASE_QUANTITY } from './types';
+
+export const productQuantity = (action, name) => {
+  return (dispatch) => {
+    console.log('inside prod qty');
+  }
+}
