cb58755aa32d6af447ab435588e8f4a70a85bd8d
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-24 17:21:26 -0400

    edit spacing of description in About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,11 @@ const About = () => {
       <h1>About ShopVint</h1>
       <br/>
       <p>
-        ShopVint is a 
+      ShopVint is a simple e-commerce shopping cart type app. Apologies in advance, but the products seen here are not actually for sale, I just built this app for fun!
+      </p>
+      <br/>
+      <p>
+      Built using React and Redux, styled using CSS
       </p>
     </div>
   )
