1ffdf942654448af6f78813555d94ad778665176
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-24 10:33:34 -0400

    in productQuantity.js create and export clearProduct function

--- src/actions/productQuantity.js
+++ src/actions/productQuantity.js
@@ -2,9 +2,18 @@ import { INCREASE_QUANTITY, DECREASE_QUANTITY } from './types';
 
 export const productQuantity = (action, name) => {
   return (dispatch) => {
-    console.log('inside prod qty');
-    console.log("the action is", action)
-    console.log("the product name is", name)
+    dispatch({
+      type: action === "increase" ? INCREASE_QUANTITY : DECREASE_QUANTITY,
+      payload: name
+    })
+  }
+}
+
+export const clearProduct = (name) => {
+  return (dispatch) => {
+    
+    console.log('inside clear product');
+    console.log("product is", name);
 
     dispatch({
       type: action === "increase" ? INCREASE_QUANTITY : DECREASE_QUANTITY,
