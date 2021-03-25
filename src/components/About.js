117001988a5c3995276022c79d97bfdac97b1d29
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-25 09:51:50 -0400

    in About.js couple last edits to description

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,12 @@ const About = () => {
       <h1>About ShopVint</h1>
       <br/>
       <p>
-      ShopVint is a simple e-commerce shopping cart type app. Apologies in advance, but the products seen here are not actually for sale, I just built this app for fun!
+      ShopVint is a simple e-commerce shopping cart type app, showcasing some cool vintage sports apparel.
+      <p>Apologies in advance, but the products seen here are not actually for sale, I just built this app for fun!</p>
       </p>
       <br/>
       <p>
-      Built using React and Redux, styled using CSS
+      Built using React and Redux, styled using CSS.
       </p>
     </div>
   )
